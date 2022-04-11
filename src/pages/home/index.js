2c815092f0da50e15885dfffb84f7d9287244060
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-04-11 18:18:07 -0300

    Criado card da home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
 import { Banner } from '../../components/banner';
+import Card from '../../components/card';
+import UsersIcon from '../../components/icons/usersIcon';
 
 export default function Home() {
   return (
@@ -8,6 +10,9 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto">
         <Banner />
+        <div className="my-4 grid grid-cols-2 gap-8 px-2 md:grid-cols-3 lg:grid-cols-4">
+          <Card icon={UsersIcon} title="Acessos" />
+        </div>
       </div>
     </div>
   );
