285971b7778b203ce049a49b64dd62695523ddbb
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-04-17 00:31:18 -0300

    Adicionado tela de seleção de condominio

--- src/applicationRoutes.js
+++ src/applicationRoutes.js
@@ -7,12 +7,14 @@ import Home from './pages/home';
 import Users from './pages/users';
 import NewUser from './pages/new-user';
 import Profile from './pages/profile';
+import SelectCondo from './pages/select-condo';
 
 const ApplicationRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/selecionar-condominio" element={<SelectCondo />} />
         <Route path="/home" element={<Home />} />
         <Route path="/acessos" element={<Users />} />
         <Route path="/novo-acesso" element={<NewUser />} />
