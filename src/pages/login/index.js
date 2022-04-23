7f3ba9f6cf73393aa59d79dccdf3bcd107330829
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-04-22 21:56:22 -0300

    Ajustes interface login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -44,14 +44,14 @@ export default function Login() {
         return <Navigate to="/selecionar-condominio" replace={true} />;
       } else {
         Toaster.showError('Ops! Não foi possível encontrar seu condomínio.');
+        user.data = null;
       }
     }
   }
-  const notify = () => Toaster.showInfo('Enviado request para o backend!');
 
   return (
     <div className="grid h-screen place-items-center">
-      <div className="card flex h-full w-full max-w-md flex-col px-4 py-8 sm:px-6 md:h-auto md:px-8 lg:px-10">
+      <div className="card flex aspect-auto h-full w-full max-w-md flex-col px-4 py-8 sm:px-6 md:h-auto md:px-8 lg:px-10">
         <div className="mb-6 self-center text-xl font-light text-gray-600 sm:text-2xl">
           Entrar na sua conta
         </div>
@@ -90,19 +90,11 @@ export default function Login() {
             <div className="flex w-full">
               <button
                 type="submit"
-                className="w-full rounded-lg  bg-purple-600 py-2 px-4 text-center text-base font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2  focus:ring-offset-purple-200"
-                onClick={notify}>
+                className="w-full rounded-lg bg-purple-600 py-2 px-4 text-center text-base font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2  focus:ring-offset-purple-200">
                 Entrar
               </button>
             </div>
           </form>
-        </div>
-        <div className="mt-6 flex items-center justify-center">
-          <a
-            href="/"
-            className="inline-flex items-center text-center text-xs font-thin text-gray-500 hover:text-gray-700">
-            <span className="ml-2">Ainda não possui uma conta?</span>
-          </a>
         </div>
       </div>
     </div>
