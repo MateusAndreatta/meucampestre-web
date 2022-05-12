f27b43d450ef697cde60746dd322f50700fe353c
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-05-12 18:32:33 -0300

    Rotas privadas

--- src/applicationRoutes.js
+++ src/applicationRoutes.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
 
 import Login from './pages/login';
 import Error404 from './pages/erros/error404';
@@ -8,18 +14,38 @@ import Users from './pages/users';
 import NewUser from './pages/new-user';
 import Profile from './pages/profile';
 import SelectCondo from './pages/select-condo';
+import store from 'store';
+
+function loggedIn() {
+  return store.get('token');
+}
+function PrivateOutlet() {
+  return loggedIn() ? <Outlet /> : <Navigate to="/" />;
+}
 
 const ApplicationRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/selecionar-condominio" element={<SelectCondo />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/acessos" element={<Users />} />
-        <Route path="/novo-acesso" element={<NewUser />} />
-        <Route path="/editar-acesso/:documento" element={<NewUser />} />
-        <Route path="/perfil" element={<Profile />} />
+        <Route path="/selecionar-condominio" element={<PrivateOutlet />}>
+          <Route path="" element={<SelectCondo />} />
+        </Route>
+        <Route path="/home" element={<PrivateOutlet />}>
+          <Route path="" element={<Home />} />
+        </Route>
+        <Route path="/acessos" element={<PrivateOutlet />}>
+          <Route path="" element={<Users />} />
+        </Route>
+        <Route path="/novo-acesso" element={<PrivateOutlet />}>
+          <Route path="" element={<NewUser />} />
+        </Route>
+        <Route path="/editar-acesso/:documento" element={<PrivateOutlet />}>
+          <Route path="" element={<NewUser />} />
+        </Route>
+        <Route path="/perfil" element={<PrivateOutlet />}>
+          <Route path="" element={<Profile />} />
+        </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
