55a7f66de6220fe75d7a947ab53a6922ebf055d2
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-05-28 09:52:31 -0300

    Adicionado cards na home

---
+++ src/components/icons/collection.js
@@ -0,0 +1,19 @@
+import { CollectionIcon as IconSolid } from '@heroicons/react/solid';
+import { CollectionIcon as IconOutline } from '@heroicons/react/outline';
+
+export default function CollectionIcon(props) {
+  let width = props.width || 'w-5';
+  let height = props.height || 'h-5';
+  let color = props.color || 'default-icon-color';
+  let type = props.type || 'outline';
+  let classes = props.className;
+
+  let style = `${width} ${height} ${color} ${classes}`;
+
+  switch (type) {
+    case 'outline':
+      return <IconOutline className={style} />;
+    default:
+      return <IconSolid className={style} />;
+  }
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Navbar from '../../components/navbar';
 import { Banner } from '../../components/banner';
 import Card from '../../components/card';
-import UsersIcon from '../../components/icons/usersIcon';
 import { Link } from 'react-router-dom';
+import UsersIcon from '../../components/icons/usersIcon';
+import CollectionIcon from '../../components/icons/collection';
+import HomeIcon from '../../components/icons/home';
 
 export default function Home() {
   return (
@@ -14,6 +16,12 @@ export default function Home() {
         <div className="my-4 grid grid-cols-2 gap-8 px-2 md:grid-cols-3 lg:grid-cols-4">
           <Link to="/acessos">
             <Card icon={UsersIcon} title="Acessos" />
+          </Link>
+          <Link to="/perfil-condominio">
+            <Card icon={HomeIcon} title="Condominio" />
+          </Link>
+          <Link to="/unidades">
+            <Card icon={CollectionIcon} title="Unidades" />
           </Link>
         </div>
       </div>
