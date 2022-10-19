a0adde910469a15cb417652c70233a6e8efdb9a1
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-10-19 20:58:16 -0300

    Ajuste nomes das variaveis de ambiente

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,7 @@
-export const PRODUCTION = process.env.PRODUCTION;
+export const PRODUCTION = process.env.REACT_APP_PRODUCTION;
 
 export const SERVER_ADDRESS = PRODUCTION
-  ? process.env.SERVER_ADDRESS
+  ? process.env.REACT_APP_SERVER_ADDRESS
   : 'http://localhost:8080';
 
 export const API_ENDPOINT = `${SERVER_ADDRESS}/api/v2`;
